1e3a6aee0d88657298a57e2ca5e80af1e61ddee1
Tanvir Tazul Islam <tanvirtazul@gmail.com>
2024-02-26 16:15:37 +0600

    Update Pointer.cpp

--- Pointer.cpp
+++ Pointer.cpp
@@ -10,20 +10,19 @@ int main() {
     int *ptr = &a; // pointer "ptr" is pointing variable "a"
     
     // cout << ptr << endl;
-    
     // cout << *ptr << endl;
     
     *ptr = 50; // changing the value of variable "a" 
     
     cout << a << endl;
     
-    int **ptr1 = &ptr; // "ptr1" is another pointer which is pointing to another pointer
+    int **ptr1 = &ptr; // "ptr1" is a pointer which is pointing to an existing pointer "ptr" (pointer to pointer)
     
     // cout << &ptr << endl;
     // cout << ptr1 << endl;
-    
     // cout << a << endl;
-    **ptr1 = 500;
+
+    **ptr1 = 500; // updating the value of "a" again
   
     cout << a << endl;
         
